851509e60d78dea620576db4564d48fa017d70b0
srvarma7 <48859937+srvarma7@users.noreply.github.com>
2023-01-09 14:08:36 +0530

    if check fix

--- HMSegmentedControl/HMSegmentedControl.m
+++ HMSegmentedControl/HMSegmentedControl.m
@@ -725,7 +725,7 @@
         }
         
 	// Removed the check for sending notification even on seleting same index.
-        if segment < sectionsCount {
+        if (segment < sectionsCount) {
             // Check if we have to do anything with the touch event
             if (self.isTouchEnabled)
                 [self setSelectedSegmentIndex:segment animated:self.shouldAnimateUserSelection notify:YES];
